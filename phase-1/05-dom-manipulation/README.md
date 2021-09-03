ac5f69b6ed567f99e2c69b0ac88c9657042626f5
Devin Mullin <devin.mullin90@gmail.com>
2021-09-03 13:11:48 -0500

    ok

--- phase-1/05-dom-manipulation/README.md
+++ phase-1/05-dom-manipulation/README.md
@@ -6,3 +6,17 @@
 - Manipulate the DOM by using: - .querySelector() - .querySelectorAll() - .createElement() - .getElementById() - .getElementsByClassName() - .appendChild() - .remove() - .innerHTML - .innerText, and .textContent
   to create, read, update, and delete elements from the DOM
 - Create and append new elements to the DOM
+
+
+// id: #idName - unique
+class: .className 
+element: elementName, p 
+
+use defer in the js link script , else js may execute functions based on incomplete code
+
+DOM: document object model
+- copy of HTML file created when server sends HTML to the browser
+- separate instance
+
+// access elements and select them
+
--- phase-1/05-dom-manipulation/index.js
+++ phase-1/05-dom-manipulation/index.js
@@ -29,4 +29,102 @@ const pokemons = [
     img: "https://static.wikia.nocookie.net/pokemon-quest-adventures/images/a/a4/005Charmeleon_XY_anime_2.png/revision/latest?cb=20170313041459",
     likes: 11,
   },
+
+  {
+    id: 6,
+    name: "devy",
+    img: "https://i1.endoftheinter.net/i/n/41ef606fc9ee18903e885eecb7d282ea/587486.jpg",
+    likes: 1000000
+  },
 ];
+
+// access elements and select them
+const pokeContainer = document.querySelector('#poke-container')
+
+const pokeForm = document.getElementById
+('poke-form')
+///console.log(pokeForm)
+
+// document.querySelector()
+const goalsDiv = document.querySelector
+('#pokeContainer')
+console.log(goalsDiv)
+
+//document-querySelectorAll()
+// returns a collection of elements that match the selector
+// .forEach can be used on this collection
+// returns a NodeList
+// !!! look into nodeList vs HTMLcollection
+
+const allDivs = document.querySelectorAll('div')
+console.log(allDivs)
+
+// change something based on what our user actually did with the application>
+    // access element before doing anything else with it
+     
+
+// creating elements
+
+pokemons.forEach(function(pokemon){
+  renderPokemon(pokemon)
+})
+
+// pass each object to another method
+
+// pokemons.forEach(renderPokemon)
+//^^can wrap it as so
+
+function renderPokemon(character){
+  // create elements that display our characters properties to the DOM
+// accept item by creating new parameter (character) 
+              //console.log to check work'
+
+// create a div
+const pokeCard = document.createElement('div')
+pokeCard.id = `poke-${character.id}`  // id here is separate from one represented on DOM
+pokeCard.className = "poke-card"
+
+const pokeImg = document.createElement('img')
+pokeImg.src = character.img                        /// created image element
+pokeImg.alt = `${character.name} image`
+
+//adding pokeCard to pokeContainer
+
+pokeCard.appendChild(pokeImg)                       /// appended img element to the cards
+pokeContainer.appendChild(pokeCard)
+}
+
+// document.createElement()
+  // creates nodes
+
+              // .innerHTML (can produce security issues)
+                // creates strings that represent the element
+
+
+// updating elements
+  // select element to change, set it equal to new value
+const header = document.querySelector('#header')
+header.append = `<img id="header-img"
+src="https://m.media-amazon.com/images/M/MV5BNzlhYjEzOGItN2MwNS00ODRiLWE5OTItYThiNmJlMTdmMzgxXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg"/>`
+
+const headerImg = document.createElement('img')
+const h1 = document.querySelector('h1')
+console.log(h1)
+
+
+
+// remove elements from our DOM
+
+// .remove() called on the targeted element
+const lectureGoals = document.querySelector('#lecture-goals')
+lectureGoals.remove()
+
+const h3 = document.createElement("h3")
+const pika = document.createTextNode("Pikachu")
+h3.appendChild(pika)
+
+pokeCard.appendChild(h3)
+
+var btn = document.createElement("button");
+btn.className = "like-bttn"
+btn.createTextNode = "♥"
